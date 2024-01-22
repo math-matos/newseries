9c1cf84fe3787de0e7a061092f0286b26912734f
Matheus Matos Bernardo <matheus.bernardoo1001@gmail.com>
2024-01-22 09:57:47 -0300

    branch nova

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import logoImg from '../assets/Vector.svg';
+
+export function Header() {
+    return (
+        <header>
+            <nav id='navbar'>
+                <div className="nav-brand">
+                    <img src={logoImg} alt="" />
+                    <h1>NEWSERIES</h1>
+                </div>
+
+                <ul className="nav-list">
+                    <li><a href="/">Séries Nacionais</a></li>
+                    <li><a href="/">Séries Internacionais</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+
