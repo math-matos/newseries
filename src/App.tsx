784f1739ddc76b900b4efe83dff57a9d9578bf6c
Matheus Matos Bernardo <matheus.bernardoo1001@gmail.com>
2024-01-22 11:10:42 -0300

    add header + more

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { Noticia } from "./pages/Noticia";
 
 function App() {
   return (
     <>
       <Header />
+      <Noticia />
     </>
   );
 }
---
+++ src/pages/Noticia.tsx
@@ -0,0 +1,35 @@
+import "../components/Noticia.css";
+
+export const Noticia = (): JSX.Element => {
+  return (
+    <div className="noticia">
+      <div className="div">
+        <img className="principal-image" alt="imagem principal" src="./public/slide2.svg" />
+        <div className="group">
+          <p className="text-wrapper">Worem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p className="por-fulano-silva">
+            <span className="span">Por Fulano Silva - 01/01/1990 - 12h00 em </span>
+            <span className="text-wrapper-2">Séries Nacionais</span>
+          </p>
+          <p className="dorem-ipsum-dolor">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus
+            sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat
+            lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent
+            auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus
+            nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam
+            in elementum tellus.
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Curabitur tempor quis
+            eros tempus lacinia. Nam bibendum pellentesque quam a convallis. Sed ut vulputate nisi. Integer in felis sed
+            leo vestibulum venenatis. Suspendisse quis arcu sem. Aenean feugiat ex eu vestibulum vestibulum. Morbi a
+            eleifend magna. Nam metus lacus, porttitor eu mauris a, blandit ultrices nibh. Mauris sit amet magna non
+            ligula vestibulum eleifend. Nulla varius volutpat turpis sed lacinia. Nam eget mi in purus lobortis
+            eleifend. Sed nec ante dictum sem condimentum ullamcorper quis venenatis nisi. Proin vitae facilisis nisi,
+            ac posuere leo.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
