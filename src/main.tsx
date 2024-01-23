8f9ffa67cd6fc2a041cb00dfead0c7c7a7bfaa26
Matheus Matos Bernardo <matheus.bernardoo1001@gmail.com>
2024-01-23 10:20:33 -0300

    add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App.tsx";
+import Portal from "./pages/Portal/Portal.tsx";
+import { Noticia } from "./pages/Noticia/Noticia.tsx";
+import { Search } from "./pages/Search/Search.tsx";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Portal />}/>
+          <Route path="/noticia" element={<Noticia />}/>
+          <Route path="search" element={<Search />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
