cf0c5023ebcf303dfd0af3068f20ff8899d39beb
Matheus Matos Bernardo <matheus.bernardoo1001@gmail.com>
2024-01-24 11:29:05 -0300

    fixing API

--- src/components/portal/FeedSeries.tsx
+++ src/components/portal/FeedSeries.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import styles from './FeedSeries.module.css';
-import CardNoticia from '../CardNoticia';
-import { Link } from 'react-router-dom';
-import useFetch from '../../hooks/useFetch';
+import { useState } from "react";
+import styles from "./FeedSeries.module.css";
+import CardNoticia from "../CardNoticia";
+import { Link } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 type Props = {
   title: string;
@@ -15,25 +15,25 @@ const FeedSeries = ({ title }: Props) => {
 
   const classeBotaoMenos =
     series && itensRenderizados[0] === 0
-      ? 'botaoMenosDesativado'
-      : 'botaoMenos';
+      ? "botaoMenosDesativado"
+      : "botaoMenos";
   const classeBotaoMais =
     series && itensRenderizados[1] >= series.length
-      ? 'botaoMaisDesativado'
-      : 'botaoMais';
+      ? "botaoMaisDesativado"
+      : "botaoMais";
 
   const seriesRenderizadas =
     series && series.slice(itensRenderizados[0], itensRenderizados[1]);
 
   const mostrarSeries = (valor: string) => {
     if (itensRenderizados[1] < series!.length) {
-      if (valor === 'mais') {
+      if (valor === "mais") {
         const novosItens = itensRenderizados.map((item) => item + 3);
         setItensRenderizados(novosItens);
       }
     }
 
-    if (valor === 'menos') {
+    if (valor === "menos") {
       if (itensRenderizados[0] > 3) {
         const novosItens = itensRenderizados.map((item) => item - 3);
         setItensRenderizados(novosItens);
@@ -43,7 +43,7 @@ const FeedSeries = ({ title }: Props) => {
     }
   };
 
-  const image_path = 'https://image.tmdb.org/t/p/w500/';
+  const image_path = "https://image.tmdb.org/t/p/w500/";
 
   return (
     <div>
@@ -67,13 +67,13 @@ const FeedSeries = ({ title }: Props) => {
       <div className={styles.botoesContainer}>
         <button
           className={`${styles[classeBotaoMenos]}`}
-          onClick={() => mostrarSeries('menos')}
+          onClick={() => mostrarSeries("menos")}
         >
           <img src="/public/arrowLeft.svg" alt="" />
         </button>
         <button
           className={`${styles[classeBotaoMais]}`}
-          onClick={() => mostrarSeries('mais')}
+          onClick={() => mostrarSeries("mais")}
         >
           <img src="/public/arrowRight.svg" alt="" />
         </button>
--- src/pages/Noticia/Noticia.tsx
+++ src/pages/Noticia/Noticia.tsx
@@ -1,6 +1,6 @@
-import { useParams } from 'react-router-dom';
-import './Noticia.css';
-import useFetch from '../../hooks/useFetch';
+import { useParams } from "react-router-dom";
+import "./Noticia.css";
+import useFetch from "../../hooks/useFetch";
 
 export const Noticia = () => {
   const { id } = useParams();
@@ -15,19 +15,29 @@ export const Noticia = () => {
         <img
           className="principal-image"
           alt="imagem principal"
-          // src={`https://image.tmdb.org/t/p/w500/${
-          //   serieSelecionada!.poster_path
-          // }`}
+          src={`https://image.tmdb.org/t/p/w500/zZDkgOmFMVYpGAkR9Tkxw0CRnxX.jpg`}
+          // configurar com a API {`https://image.tmdb.org/t/p/w500/${serieSelecionada!.backdrop_path}`}
         />
         <div className="container">
-          {/* <p className="text-wrapper">{serieSelecionada?.name}</p> */}
+          <p className="text-wrapper">
+            Série de artes marciais continua rivalidade de Karate Kid  {/* // configurar com a API {serieSelecionada?.name} */}
+          </p>
           <p className="por-fulano-silva">
             <span className="span-1">
-              Por Fulano Silva - 01/01/1990 - 12h00 em{' '}
+              Por Fulano Silva - 01/01/1990 - 12h00 em{" "}
             </span>
             <strong>Séries Nacionais</strong>
           </p>
-          {/* <p className="dorem-ipsum-dolor">{serieSelecionada?.overview}</p> */}
+          <p className="dorem-ipsum-dolor">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit,
+            incidunt repellendus consectetur harum eum maxime consequatur
+            delectus iste earum? Dolore.
+            <br />
+            <br />
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa ipsa
+            labore itaque provident eligendi. Pariatur est mollitia maxime
+            delectus non? {/* // configurar com a API {serieSelecionada?.overview} */}
+          </p>
         </div>
       </div>
 
