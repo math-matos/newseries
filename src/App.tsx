5044d5b20a1ecf65264e95ac657ab421dbe5617d
Vinicius Meirelles <vinicius_meirelless@hotmail.com>
2024-01-30 11:28:11 -0300

    iniciada página com renderização de listas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import Noticia from "./pages/Noticia/Noticia";
-import Portal from "./pages/Portal/Portal";
-import { Navbar } from "./components/Navbar";
+import { Route, Routes } from 'react-router-dom';
+import Noticia from './pages/Noticia/Noticia';
+import Portal from './pages/Portal/Portal';
+import { Navbar } from './components/Navbar';
+import Lista from './pages/Lista/Lista';
+import { ThemeProvider } from '@emotion/react';
+import { Theme } from './themes/Theme';
+import { responsiveFontSizes } from '@mui/material';
 
 function App() {
+  const theme = responsiveFontSizes(Theme);
   return (
     <>
-      <Navbar />
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Portal />} />
-        <Route path="/serie/:id" element={<Noticia />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Portal />} />
+          <Route path="/serie/:id" element={<Noticia />} />
+          <Route path="/avaliacao" element={<Lista />} />
+        </Routes>
+      </ThemeProvider>
     </>
   );
 }
