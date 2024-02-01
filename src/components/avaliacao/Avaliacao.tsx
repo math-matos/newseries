7131647f5f8b43e777a14afb60dbe9ecb36296c1
Matheus Matos Bernardo <matheus.bernardoo1001@gmail.com>
2024-02-01 16:02:20 -0300

    fixing comentarios area

--- src/components/avaliacao/Avaliacao.tsx
+++ src/components/avaliacao/Avaliacao.tsx
@@ -1,5 +1,8 @@
 import { Autocomplete, Stack } from "@mui/material";
 
+import { TextareaAutosize as BaseTextareaAutosize } from "@mui/base/TextareaAutosize";
+import { styled } from "@mui/system";
+
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Rating from "@mui/material/Rating";
@@ -13,6 +16,62 @@ import { useState } from "react";
 
 export const Avaliacao = () => {
   const [value, setValue] = useState<number | null>(2);
+
+  const blue = {
+    100: "#DAECFF",
+    200: "#b6daff",
+    400: "#3399FF",
+    500: "#007FFF",
+    600: "#0072E5",
+    900: "#003A75",
+  };
+
+  const grey = {
+    50: "#F3F6F9",
+    100: "#E5EAF2",
+    200: "#DAE2ED",
+    300: "#C7D0DD",
+    400: "#B0B8C4",
+    500: "#9DA8B7",
+    600: "#6B7A90",
+    700: "#434D5B",
+    800: "#303740",
+    900: "#1C2025",
+  };
+
+  const Textarea = styled(BaseTextareaAutosize)(
+    ({ theme }) => `
+    box-sizing: border-box;
+    width: 100%;
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-size: 1.5rem;
+    line-height: 1.5;
+    padding: 8px 12px;
+    border-radius: 8px;
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+    background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+    border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
+    box-shadow: 0px 2px 2px ${
+      theme.palette.mode === "dark" ? grey[900] : grey[50]
+    };
+
+    &:hover {
+      border-color: ${blue[400]};
+    }
+
+    &:focus {
+      border-color: ${blue[400]};
+      box-shadow: 0 0 0 3px ${
+        theme.palette.mode === "dark" ? blue[600] : blue[200]
+      };
+    }
+
+    // firefox
+    &:focus-visible {
+      outline: 0;
+    }
+  `
+  );
 
   return (
     <div className="overlap-wrapper">
@@ -104,13 +163,7 @@ export const Avaliacao = () => {
                 noValidate
                 autoComplete="off"
               >
-                <TextField
-                  fullWidth
-                  style={{}}
-                  id="filled-basic"
-                  label="Escreva aqui o seu comentário"
-                  variant="filled"
-                />
+                <Textarea minRows={7} />
               </Box>
             </Stack>
           </div>
